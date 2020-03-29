ca9aa760d0b37c3e98980e65180a3c05d7879e49
MattMan569 <matthewpolsom@hotmail.com>
2020-03-29 16:53:18 -0600

    setup mongodb

--- server/app.js
+++ server/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to database");
+}).catch((reason) => {
+  console.log("Connection to database failed:\n", reason);
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,34 +26,42 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json(post);
+app.post("/api/posts", async (req, res, next) => {
+  try {
+    const post = await new Post({
+      title: req.body.title,
+      content: req.body.content,
+    }).save({ new: true });
+
+    res.status(201).json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
 });
 
-app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "jdhas352738h",
-      title: "First server-side post",
-      content: "Server post"
-    },
-    {
-      id: "453897yho5tuonuqw",
-      title: "Second server-side post",
-      content: "Server post 2"
-    }
-  ];
-
-  res.json({
-    message: "Success",
-    posts
-  });
+app.get("/api/posts", async (req, res, next) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
 });
+
+app.delete("/api/posts/:id", async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    res.send(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+})
 
 module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -15,10 +16,13 @@ export class PostsService {
 
   // Return a copy of the posts array
   getPosts() {
-    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts').subscribe((postData) => {
-      this.posts = postData.posts;
-      this.postsUpdated.next([...this.posts]);
-    });
+    this.http.get<Post[]>('http://localhost:3000/api/posts', { observe: 'response' })
+      .subscribe((response) => {
+        this.posts = response.body;
+        this.postsUpdated.next([...this.posts]);
+      }, (error: HttpErrorResponse) => {
+        console.log(error);
+      });
   }
 
   // Listen for post updates
@@ -28,10 +32,21 @@ export class PostsService {
 
   // Add a post and emit an update event
   addPost(title: string, content: string) {
-    const post: Post = { id: null, title, content };
-    this.http.post<Post>('http://localhost:3000/api/posts', post).subscribe((postData) => {
-      this.posts.push(postData);
-      this.postsUpdated.next(this.posts);
+    const post: Post = { _id: null, title, content };
+
+    this.http.post<Post>('http://localhost:3000/api/posts', post, { observe: 'response' }).subscribe((response) => {
+      this.posts.push(response.body);
+      this.postsUpdated.next([...this.posts]);
+    });
+  }
+
+  deletePost(id: string) {
+    this.http.delete<Post>(`http://localhost:3000/api/posts/${id}`, { observe: 'response' }).subscribe((response) => {
+      this.posts = this.posts.filter((el) => {
+        return el._id !== response.body._id;
+      });
+
+      this.postsUpdated.next([...this.posts]);
     });
   }
 }
